357ad3f5435d4bd580774e8bfd22c68dd4df353d
gabrielmarinodasilva <gabigol12082004@gmail.com>
2025-05-25 17:40:38 -0300

    back-end

---
+++ server.js
@@ -0,0 +1,88 @@
+import express  from 'express'
+import { PrismaClient } from './generated/prisma/index.js'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', async (req, res) => {
+
+
+    await prisma.user.create({
+        data: {
+            nome:          req.body.nome,
+            biografia:     req.body.biografia,
+            idade:         req.body.idade,
+            nacionalidade: req.body.nacionalidade
+        }
+    })
+
+    res.status(201).json(req.body)
+
+}) 
+
+app.get('/usuarios', async (req, res) => {
+
+    let users = []
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                nome:          req.query.nome,
+                biografia:     req.query.biografia,
+                idade:         req.query.idade,
+                nacionalidade: req.query.nacionalidade
+            }
+        })
+
+    }else {
+        const users = await prisma.user.findMany()
+    }
+    
+    
+    
+    
+    
+    res.status(200).json(users)
+
+
+
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            nome:          req.body.nome,
+            biografia:     req.body.biografia,
+            idade:         req.body.idade,
+            nacionalidade: req.body.nacionalidade
+        }
+    })
+
+    res.status(201).json(req.body)
+
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({message: 'usuarios deletado'})
+})
+
+app.listen(3000)
+
+/*
+banco: ibiel
+        admin
+*/
